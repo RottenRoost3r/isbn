38c54a09bf26bc16e32e3e6ece0ba0044ac27bd1
Jordan Endicott <jordan.endicott823@gmail.com>
2018-08-13 10:31:47 -0400

    length checker checks length

--- tests/test_isbn.rb
+++ tests/test_isbn.rb
@@ -12,4 +12,10 @@ class TestIsbn < Minitest::Test
         assert_equal("12345", isbn_dash("1 - 23-4 5"))
         assert_equal("12345", isbn_dash("1  -  2  -  3  -  4  -  5"))
     end
+
+    def test_that_length_checker_works
+        assert_equal(false, length_checker("1234567"))
+        assert_equal(false, length_checker("1-2-3-4-5-6-7"))
+        assert_equal(false, length_checker("1 -2 -3 -4 -5 -6 -7 -"))
+    end
 end
