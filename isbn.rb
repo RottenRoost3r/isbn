045c8e6e3ca77245eb07a7691faad733179333c8
Jordan Endicott <jordan.endicott823@gmail.com>
2018-08-13 14:36:06 -0400

    finished functions, didn't push enough

--- isbn.rb
+++ isbn.rb
@@ -4,4 +4,67 @@ def isbn_dash(num)
     num = num.tr(' ', '')
     num
 end
-    
+
+def funk_master(num)
+    num2 = isbn_dash(num)
+    chek = num2.length
+    if chek > 10
+        num2 = isbn_13(num2)
+    elsif chek == 10
+        num2 = isbn_10(num2)
+    else
+        return false
+    end
+    num2
+end
+
+def isbn_10(num2)
+    num3 = num2.split('')
+    if num3.last.downcase == "x"
+        return true
+    else
+        counter = 10
+        check_arr = []
+        num3.each.with_index(1) do |n, index|
+            chk = n.to_i*counter
+            counter -= 1
+            check_arr << chk
+        end
+        check_num = check_arr.sum
+        check_num = check_num % 11
+        check_num
+        if check_num == 0
+            return true
+        else
+            return false
+        end
+    end
+end
+
+def isbn_13(num2)
+    num3 = num2.split('')
+    var = num3.pop
+    teen_arr = []
+    num3.each.with_index(1) do |n, index|
+        if index.even?
+            v = n.to_i*3
+            teen_arr << v
+        else 
+            m = n.to_i*1
+            teen_arr << m
+        end                
+    end
+    check = teen_arr.sum
+    check %= 10
+    check = 10 - check
+    check
+    if check.to_s.length == 2
+        check %= 10
+    end
+    if check.to_s == var
+        return true
+    else
+        return false
+    end
+end
+
