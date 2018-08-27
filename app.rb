34d469b02f5d8912552aaa9d576b273480739722
Jordan Endicott <jordan.endicott823@gmail.com>
2018-08-27 13:04:54 -0400

    finished bucket

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
 require 'sinatra'
+require 'csv'
+require 'aws-sdk-s3'
+require 'json'
 require_relative 'isbn.rb'
 # require_relative 'csv.rb'
-require 'csv'
+
 enable :sessions
+
+load 'local_ENV.rb' if File.exist?('local_ENV.rb')
+
+s3 = Aws::S3::Client.new(profile: 'rottenrooster', region: 'us-east-2')
 
 get '/' do
   result = session[:result] || []
@@ -19,9 +26,18 @@ post'/isbn' do
     line_arr << n
   end
 
+  valid = CSV.generate do |csv|
+    val_arr.each_with_index do |v,i|
+      csv << [line_arr[i], v]
+    end
+  end
+
   session[:result] = val_arr
   session[:isbn_name] = line_arr
   p "result is #{val_arr}"
   p "name is #{line_arr}"
+
+  s3.put_object(bucket:'j823bucket', body: valid, key:"output_isbn_file.csv")
+
   redirect "/"
 end
